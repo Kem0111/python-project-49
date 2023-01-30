8abd5b9c2c300e598583aa2c41e48a7bd511df3f
diyeqo@icloud.com <saidavelieva@192.168.1.21>
2023-01-30 13:53:00 +0300

    add new scripts

--- brain_games/logic.py
+++ brain_games/logic.py
@@ -1,5 +1,6 @@
 import prompt
-from random import randint
+from random import randint, choice
+from operator import add, sub, mul
 
 
 def question():
@@ -30,3 +31,29 @@ Answer "yes" if the number is even, otherwise answer "no"''')
             break
     if a == 0:
         print(f'Congratulations, {name}')
+
+
+def question_for_calc():
+    a = randint(1, 100)
+    b = randint(1,100)
+    get_operator = {'+': add, '-': sub, '*': mul}.get
+    operator = choice(('+', '-', '*'))
+    result = get_operator(operator)
+    expression = [a, operator, b]
+    string = ''
+    integer = result(a, b)
+    for i in expression:
+        string += str(i) + ' '   
+    print(f'Question: {string}')
+    answer = prompt.string('Your answer: ')
+    if str(answer) == str(integer):
+        print('Correct!')
+        return True
+    else:
+        print(f"'{answer}' is wrong answer ;(. Correct answer was '{integer}'")
+        return False
+
+
+
+
+
