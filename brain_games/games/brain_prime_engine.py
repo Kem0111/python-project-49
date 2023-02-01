badc51a1f9d527787dea6c9ef8b76a60d52a22b8
diyeqo@icloud.com <saidavelieva@192.168.1.21>
2023-02-01 16:15:37 +0300

    update brain-prime

--- brain_games/games/brain_prime_engine.py
+++ brain_games/games/brain_prime_engine.py
@@ -2,9 +2,7 @@ import prompt
 from random import randint
 
 
-def is_br_prime():
-    print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    number = randint(1, 100)
+def is_br_prime(number):
     num = 0
     if number == 1:
         num += 1
@@ -12,12 +10,17 @@ def is_br_prime():
         if number % i == 0 and i < number:
             num += 1
             break
+    return num
+
+
+def br_prime():
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    number = randint(1, 100)
     print(f'Question: {number}')
     answer = prompt.string('Your answer: ')
-    if num == 0 and answer == 'yes':
-        print('Correct!')
-        return True
-    elif num != 0 and answer == 'no':
+    num = is_br_prime(number)
+    if num == 0 and answer == 'yes' or \
+       num != 0 and answer == 'no':
         print('Correct!')
         return True
     elif num == 0 and answer != 'yes':
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.games.brain_prime_engine import is_br_prime
+from brain_games.games.brain_prime_engine import br_prime
 from brain_games.logic import questions
 
 
 def main():
-    questions(is_br_prime)
+    questions(br_prime)
 
 
 if __name__ == '__main__':
