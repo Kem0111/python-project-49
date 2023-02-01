e31807083c4a9e24fdc8bf06a06f1a5d0bcea3d7
diyeqo@icloud.com <saidavelieva@192.168.1.21>
2023-02-01 14:57:16 +0300

    fix

--- brain_games/games/brain_progression_engine.py
+++ brain_games/games/brain_progression_engine.py
@@ -6,7 +6,7 @@ def br_progression():
     print('What number is missing in the progression?')
     progression = list(range(randint(1, 30), randint(70, 100),
                              randint(2, 10)))[:10]
-    index = randint(0, len(progression)-1)
+    index = randint(0, len(progression) - 1)
     random_int = progression[index]
     progression[index] = '..'
     string_progression = ' '.join([str(i) for i in progression])
