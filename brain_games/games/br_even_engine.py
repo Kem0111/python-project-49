f773067b5925b3df1e9fc0758bd6a7a2b648760d
diyeqo@icloud.com <saidavelieva@192.168.1.21>
2023-02-01 14:27:51 +0300

    fix brain-even

--- brain_games/games/br_even_engine.py
+++ brain_games/games/br_even_engine.py
@@ -3,7 +3,7 @@ import prompt
 
 
 def br_even():
-    print('Answer "yes" if the number is even, otherwise answer "no"')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
     random_number = randint(1, 100)
     print(f'Question: {random_number}')
     answer = prompt.string('Your answer: ')
@@ -12,8 +12,8 @@ def br_even():
         print('Correct!')
         return True
     elif random_number % 2 == 0 and answer != 'yes':
-        print(f"'{answer}' is wrong answer ;(. Correct answer was 'yes'")
+        print(f"'{answer}' is wrong answer ;(. Correct answer was 'yes'.")
         return False
     elif random_number % 2 != 0 and answer != 'no':
-        print(f"'{answer}' is wrong answer ;(. Correct answer was 'no'")
+        print(f"'{answer}' is wrong answer ;(. Correct answer was 'no'.")
         return False
