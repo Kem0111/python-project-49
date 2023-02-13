b0a141a246e82acb48ba9a36c16587b45c6e7d82
diyeqo@icloud.com <saidavelieva@192.168.1.21>
2023-02-14 01:57:32 +0300

    fix

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -4,9 +4,9 @@ from brain_games.games.br_gcd_engine import br_gcd,\
     rules_br_gcd, get_true_answer_br_gcd
 
 
-def main_br_gcd():
+def main():
     questions(br_gcd, rules_br_gcd, get_true_answer_br_gcd)
 
 
 if __name__ == '__main__':
-    main_br_gcd()
+    main()
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -4,9 +4,9 @@ from brain_games.games.brain_prime_engine import br_prime,\
 from brain_games.logic import questions
 
 
-def main_br_prime():
+def main():
     questions(br_prime, rules_br_prime, get_true_answer_br_prime)
 
 
 if __name__ == '__main__':
-    main_br_prime()
+    main()
