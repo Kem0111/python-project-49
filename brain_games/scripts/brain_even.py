1fbadc2f5dff9bb813c552103c3e5592d986278a
diyeqo@icloud.com <saidavelieva@192.168.1.21>
2023-02-14 02:26:13 +0300

    fix

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-from brain_games.logic import questions
 from brain_games.games.br_even_engine import\
     br_even, rules_br_even, get_true_answer_br_even
+from brain_games.logic import questions
 
 
 def main():
